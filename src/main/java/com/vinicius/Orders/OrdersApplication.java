430ded5164417439c80fc3eb8d71e3685acc5e56
Vinicius Fernandes <vinicius.fernandes.lima@outlook.com>
2023-06-12 20:01:40 -0300

    Projeto finalizado

--- src/main/java/com/vinicius/Orders/OrdersApplication.java
+++ src/main/java/com/vinicius/Orders/OrdersApplication.java
@@ -2,6 +2,7 @@ package com.vinicius.Orders;
 
 import com.vinicius.Orders.entities.Order;
 import com.vinicius.Orders.services.impl.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +14,7 @@ import java.util.Scanner;
 @SpringBootApplication
 public class OrdersApplication implements CommandLineRunner {
 
+	@Autowired
 	private OrderService orderService;
 
 	public static void main(String[] args) {
